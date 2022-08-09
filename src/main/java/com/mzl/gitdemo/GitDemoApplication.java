e6a5adafe27ed43deb07a9885ea8157226697d66
mzl <2198902814@qq.com>
2022-08-09 16:30:38 +0800

    first commit

---
+++ src/main/java/com/mzl/gitdemo/GitDemoApplication.java
@@ -0,0 +1,13 @@
+package com.mzl.gitdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitDemoApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/mzl/gitdemo/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.mzl.gitdemo.controller;
+
+/**
+ * @ClassName :   TestController
+ * @Description: TODO
+ * @Author: mzl
+ * @CreateDate: 2022/8/9 16:25
+ * @Version: 1.0
+ */
+public class TestController {
+
+    public String master(){
+        return "master";
+    }
+
+}
